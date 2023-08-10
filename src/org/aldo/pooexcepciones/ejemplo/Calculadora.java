1c0c933ea987efff7db13a210a356863255d88cf
IanJim <95320795+IanJim@users.noreply.github.com>
2023-08-10 12:33:54 -0600

    added Calculadora y excepciones

---
+++ src/org/aldo/pooexcepciones/ejemplo/Calculadora.java
@@ -0,0 +1,8 @@
+package org.aldo.pooexcepciones.ejemplo;
+
+public class Calculadora {
+
+    public double dividir(int numerador, int divisor) {
+
+    }
+}
--- src/org/aldo/pooexcepciones/ejemplo/EjemploExcepciones.java
+++ src/org/aldo/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -1,7 +1,25 @@
 package org.aldo.pooexcepciones.ejemplo;
 
-public class Ejemplo {
+import javax.swing.*;
+
+public class EjemploExcepciones {
     public static void main(String[] args) {
-        
+
+        String valor = JOptionPane.showInputDialog("Ingrese un valor numérico: ");
+        int divisor;
+        try {
+            divisor = Integer.parseInt(valor);
+            System.out.println(10 / divisor);
+        } catch (NumberFormatException nfe) {
+            System.out.println("Se detecto una excepción: ingrese un valor numérico: " + nfe.getMessage());
+            main(args);
+        }
+        catch (ArithmeticException ae) {
+            System.out.println("Capturamos la excepción en tiempo de ejecución: " + ae.getMessage());
+            main(args);
+        } finally {
+            System.out.println("Es opcional, pero se ejecuta con excepción o sin!");
+        }
+        System.out.println("Continuamos con el flujo de nuestra aplicación");
     }
 }
